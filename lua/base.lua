a2a09afb18ffe5402c8818a87ae586d627db5758
KeaghanKennedy <keaghan.kennedy@gmail.com>
2022-08-17 20:14:43 +1000

    initial nvim config

---
+++ lua/base.lua
@@ -0,0 +1,32 @@
+-- Show relative line numbers.
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Disable error bells.
+vim.opt.errorbells = false
+
+-- Tab width.
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Scroll offset of 10
+vim.opt.scrolloff = 10
+
+vim.opt.termguicolors = true
+
+-- Increase space for displaying messages.
+vim.opt.cmdheight = 1
+
+-- Decrease update time for improved performance.
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
